845545cd259654cce2561cb9e3bf8a8b23cf9aff
Oscar-Jin <50736585+Oscar-Jin@users.noreply.github.com>
2020-10-05 15:40:56 +0900

    feat: add example page

---
+++ examples/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import OscarSaysHi from "../../src/index";
+
+const App = () => <OscarSaysHi />;
+
+ReactDOM.render(<App />, document.getElementById("root"));
